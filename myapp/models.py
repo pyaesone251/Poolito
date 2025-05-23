011f438f787b3ae4fa7dd0e869f3b4187106b0e5
pyaesone251 <pyaesonepaing06@gmail.com>
2025-05-23 12:01:10 +0630

    Initial

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class HeroAera(models.Model):
+    icon_img = models.ImageField(upload_to='hero _aera', blank=True, null=True)
+    icon_title = models.CharField(max_length=200 , blank=True, null=True)
+    bg_img = models.ImageField(upload_to='hero_aera', blank=True, null=True)
+    title = models.CharField(max_length=20 , blank=True, null=True)
+    note = models.CharField(max_length=100, blank=True, null=True)
+    video = models.URLField(null=True, blank=True)
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def indexview(request):
-    return render(request, 'index.html')
+    hero_area = HeroAera.objects.all().order_by('-id')[:1]
+    contex = {
+        'hero_area':hero_area
+    }
+    return render(request, 'index.html',contex)
