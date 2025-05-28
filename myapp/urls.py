316d1dab83ab920f8a873f31c854f2fc770990a1
pyaesone251 <pyaesonepaing06@gmail.com>
2025-05-28 10:11:28 +0630

    inital

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,9 @@ from .import views
 
 urlpatterns = [
     path('',views.indexview, name='home'),
+    path('about/',views.About_view, name='about'),
+    path('service/',views.service_view,name='service'),
+    path('blog/',views.blog_view,name='blgo'),
+    path('contact/',views.contact_view, name='conatct'),
+    path('team/',views.team_view, name='team'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,18 @@ def indexview(request):
         'hero_area':hero_area
     }
     return render(request, 'index.html',contex)
+
+def About_view(request):
+    return render(request, 'about.html')
+
+def service_view(request):
+    return render(request,'service.html')
+
+def blog_view(request):
+    return render(request, 'blog.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def team_view(request):
+    return render(request, 'team.html')
